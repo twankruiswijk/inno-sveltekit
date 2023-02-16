a30ed728f148b2eec19aadd93fb9b1e2d8d98ea1
Hernani Fernandes <hernanif1@gmail.com>
2023-02-16 15:39:39 +0100

    add isr

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,7 +1,6 @@
 import { getPostQuery } from '../../../services/blog/blog.queries';
 import type { PageLoad } from './$types';
 
-export const prerender = true;
 
 export const load: PageLoad = async ({ parent, params }) => {
 	const { queryClient } = await parent();
