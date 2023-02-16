0c54a1d172989e80907713e9943cec9495685afd
Hernani Fernandes <hernanif1@gmail.com>
2023-02-16 14:57:41 +0100

    move comments to guestbook

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,6 +1,8 @@
 import { getPostQuery } from '../../../services/blog/blog.queries';
 import type { PageLoad } from './$types';
 
+export const prerender = true;
+
 export const load: PageLoad = async ({ parent, params }) => {
 	const { queryClient } = await parent();
 
